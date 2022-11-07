84ae0c5345e15ac80b32dcd76eef5ed1366e3638
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-11-07 19:00:45 +0200

    Add: File Handling controller "empty" Add: File Handling library "empty"

---
+++ ASP-API-File-Back/Controllers/FileHandlingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_API_File_Back.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class FileHandlingController : Controller
+    {
+        
+    }
+}
---
+++ FileHandlingLibrary/FileHandling.cs
@@ -0,0 +1,30 @@
+﻿namespace FileHandlingLibrary
+{
+    public class FileHandling
+    {
+        public bool UploadFile()
+        {
+            return false;
+        }
+
+        public bool DownloadFile(string fileName)
+        {
+            return false;
+        }
+
+        public bool GetAllFilesNames()
+        {
+            return false;
+        }
+
+        public bool GetFileByName(string fileName)
+        {
+            return false;
+        }
+
+        public bool GetFileByDate(DateTime date)
+        {
+            return false;
+        }
+    }
+}
